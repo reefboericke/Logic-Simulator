72115511e61d78ef7f97993ea71068d3afd39b70
Finn Ashley <finn.ashley@cam.ac.uk>
2021-05-23 12:28:07 +0100

    make new files needed for testing

---
+++ sample_scanner_usage.py
@@ -0,0 +1,6 @@
+""" For testing usage of scanner before implementation of pytests. """
+
+from scanner import Symbol
+from scanner import Scanner
+
+scanner = Scanner(test_BNA_file.txt, )...
--- test_scanner.py
+++ test_scanner.py
@@ -1,3 +1,11 @@
 """Test the devices module."""
 import pytest
 
+from scanner import Symbol
+from scanner import Scanner
+
+@pytest.fixture:
+def new_symbol():
+    """ Returns a new symbol. """
+    return Symbol()
+
