1a3744cb9d522b638982c0085b5407b43db6ec0b
j-m-nash <68859468+j-m-nash@users.noreply.github.com>
2021-05-25 11:49:51 +0100

    make test_names pep8 compliant

--- test_names.py
+++ test_names.py
@@ -3,15 +3,18 @@ import pytest
 
 from names import Names
 
+
 @pytest.fixture
 def new_names():
     """Return a new names instance."""
     return Names()
 
+
 @pytest.fixture
 def name_string_list():
     """Return a list of example names."""
     return ["S1", "S2", "G1"]
+
 
 @pytest.fixture
 def used_names(name_string_list):
@@ -20,12 +23,14 @@ def used_names(name_string_list):
     my_name.lookup(name_string_list)
     return my_name
 
+
 def test_query_raises_exceptions(used_names):
     """Test if query raises expected exceptions."""
     with pytest.raises(TypeError):
         used_names.lookup(1)
     with pytest.raises(TypeError):
         used_names.lookup(None)
+
 
 def test_lookup_raises_exceptions(name_string_list, used_names):
     """Test if lookup raises expected exceptions."""
@@ -38,6 +43,7 @@ def test_lookup_raises_exceptions(name_string_list, used_names):
     with pytest.raises(TypeError):
         used_names.lookup(name_string_list.append(None))
 
+
 def test_get_name_string_raises_exceptions(used_names):
     """Test if get_name_string raises expected exceptions."""
     with pytest.raises(TypeError):
@@ -46,6 +52,7 @@ def test_get_name_string_raises_exceptions(used_names):
         used_names.get_name_string("hello")
     with pytest.raises(ValueError):
         used_names.get_name_string(-1)
+
 
 @pytest.mark.parametrize("name_string, expected_name_id", [
     ("S1", 0),
@@ -60,10 +67,11 @@ def test_query(used_names, new_names, name_string, expected_name_id):
     # name_string is absent
     assert new_names.query(name_string) is None
 
+
 @pytest.mark.parametrize("name_string_list, expected_name_id_list", [
     # name_strings are present
     (["S1", "S2", "G1"], [0, 1, 2]),
-    # one name_string is absent 
+    # one name_string is absent
     (["S1", "S2", "G1", "G2"], [0, 1, 2, 3]),
     # repeated name_string
     (["S1", "S1"], [0, 0])
@@ -71,6 +79,7 @@ def test_query(used_names, new_names, name_string, expected_name_id):
 def test_lookup(used_names, name_string_list, expected_name_id_list):
     """Test if lookup returns the expected string."""
     assert used_names.lookup(name_string_list) == expected_name_id_list
+
 
 @pytest.mark.parametrize("name_id, expected_name_string", [
     (0, "S1"),
