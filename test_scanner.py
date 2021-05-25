d5ae087a19069bd91a7e3552abae9429a4f07f68
Finn Ashley <finn.ashley@cam.ac.uk>
2021-05-25 10:44:08 +0100

    small test changes

--- test_scanner.py
+++ test_scanner.py
@@ -1,11 +1,20 @@
-"""Test the devices module."""
+"""Test the scanner module."""
 import pytest
 
 from scanner import Symbol
 from scanner import Scanner
 
-@pytest.fixture:
+@pytest.fixture
 def new_symbol():
     """ Returns a new symbol. """
     return Symbol()
 
+@pytest.fixture
+def new_scanner():
+    """ Returns a scanner operating on a sample BNA file """
+    return Scanner('test_BNA_file.txt', )
+
+def test_next_symbol(new_scanner):
+    next_symbol = new_scanner.get_symbol()
+
+    assert(next_symbol == 'begindevices')
