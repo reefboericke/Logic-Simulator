bf4597e9f16220d50a9184ff075cff786fe9663a
j-m-nash <68859468+j-m-nash@users.noreply.github.com>
2021-05-28 09:58:52 +0100

    Complete the rest of the EBNF definitions for syntax error detection

--- parse.py
+++ parse.py
@@ -43,38 +43,233 @@ class Parser:
         self.network = network
         self.monitors = monitors
         self.scanner = scanner
+        self.device_ids = [self.scanner.CLOCK_ID, self.scanner.SWITCH_ID, 
+         self.scanner.DTYPE_ID, self.scanner.AND_ID, self.scanner.NAND_ID,
+         self.scanner.OR_ID, self.scanner.NOR_ID, self.scanner.XOR_ID]
+        self.variable_ids = [self.scanner.inputs_ID, self.scanner.period_ID, 
+         self.scanner.initial_ID]
+        # self.output_ids = [self.scanner.Q, self.scanner.QBAR]
 
     def error(self):
-        print("error encountered")
+        print("error encountered on symbol type: ", self.currsymb.type, " and ID: ", self.currsymb.id)
+        while self.currsymb.type != self.scanner.SEMICOLON:
+            self.currsymb = self.scanner.get_symbol()
+        self.currsymb = self.scanner.get_symbol()
 
+    def monitordefinitiongrammar(self):
+        if self.currsymb.type == self.scanner.NAME:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.SEMICOLON:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+    def assignoutputgrammar(self):
+        if self.currsymb.type == self.scanner.DOT:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.NAME: # Add Q/QBAR IDs to scanner
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+    def connectiondefinitiongrammar(self):
+        if self.currsymb.type == self.scanner.NAME:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.DOT:
+            self.assignoutputgrammar()
+
+        if self.currsymb.type == self.scanner.ARROW:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.NAME:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.DOT:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.NAME: # Can we refine this to only allow inputs?
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.SEMICOLON:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+    def assignvariablegrammar(self):
+        if self.currsymb.type == self.scanner.COLON:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.id in self.variable_ids:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+        
+        if self.currsymb.type == self.scanner.EQUALS:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.NUMBER:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+
+    def devicedefinitiongrammar(self):
+        if self.currsymb.id in self.device_ids:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.NAME:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.COLON:
+            self.assignvariablegrammar()
+        
+        if self.currsymb.type == self.scanner.SEMICOLON:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+    def monitorblockgrammar(self): 
+        if self.currsymb.id == self.scanner.begin_ID:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+    
+        if self.currsymb.id == self.scanner.monitors_ID:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.COLON:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+        
+        while self.currsymb.type == self.scanner.NAME:
+            self.monitordefinitiongrammar()
+
+        if self.currsymb.id == self.scanner.end_ID:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.id == self.scanner.monitors_ID:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.SEMICOLON:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+    def connectionblockgrammar(self):
+        if self.currsymb.id == self.scanner.begin_ID:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()   
+
+        if self.currsymb.id == self.scanner.connections_ID:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.COLON:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+        
+        while self.currsymb.type == self.scanner.NAME:
+            self.connectiondefinitiongrammar()
+
+        if self.currsymb.id == self.scanner.end_ID:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.id == self.scanner.connections_ID:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+
+        if self.currsymb.type == self.scanner.SEMICOLON:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()
+    
     def deviceblockgrammar(self):
-        currsymb = self.scanner.get_symbol()
-        if currsymb.id != self.scanner.begin_ID:
+        if self.currsymb.id == self.scanner.begin_ID:
+            self.currsymb = self.scanner.get_symbol()
+        else:
+            self.error()   
+
+        if self.currsymb.id == self.scanner.devices_ID:
+            self.currsymb = self.scanner.get_symbol()
+        else:
             self.error()
-        currsymb = self.scanner.get_symbol()
-        if currsymb.id != self.scanner.devices_ID:
+
+        if self.currsymb.type == self.scanner.COLON:
+            self.currsymb = self.scanner.get_symbol()
+        else:
             self.error()
-        currsymb = self.scanner.get_symbol()
-        if currsymb.type != self.scanner.COLON:
+        
+        while self.currsymb.id in self.device_ids:
+            self.devicedefinitiongrammar()
+
+        if self.currsymb.id == self.scanner.end_ID:
+            self.currsymb = self.scanner.get_symbol()
+        else:
             self.error()
-        currsymb = self.scanner.get_symbol()
-        # self.devicedefintiongrammar()
-        if currsymb.id != self.scanner.end_ID:
+
+        if self.currsymb.id == self.scanner.devices_ID:
+            self.currsymb = self.scanner.get_symbol()
+        else:
             self.error()
-        currsymb = self.scanner.get_symbol()
-        if currsymb.id != self.scanner.devices_ID:
+
+        if self.currsymb.type == self.scanner.SEMICOLON:
+            self.currsymb = self.scanner.get_symbol()
+        else:
             self.error()
-        currsymb = self.scanner.get_symbol()
-        if currsymb.type != self.scanner.SEMICOLON:
-            self.error()
-            
 
     def BNAcodegrammar(self):
+
         self.deviceblockgrammar()
-        # self.connectionblockgrammar()
-        # self.monitorblockgrammar()
+
+        self.connectionblockgrammar()
+
+        if self.currsymb.id == self.scanner.begin_ID:
+            self.monitorblockgrammar()
 
     def parse_network(self):
         """Parse the circuit definition file."""
+        self.currsymb = self.scanner.get_symbol()
         self.BNAcodegrammar()
+
+        """ Note that currently, it correctly will allow 
+         correct files to be run, but any erros in the file
+         will be flagged as several unique errors """
         
