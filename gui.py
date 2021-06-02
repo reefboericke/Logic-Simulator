d3326f438bd26d6fa619e47b682959c095408de1
Reef Boericke <rrb33@cam.ac.uk>
2021-06-02 19:43:38 +0100

    move labels and add 0/1 scale indicators to signal traces

--- gui.py
+++ gui.py
@@ -110,7 +110,9 @@ class MyGLCanvas(wxcanvas.GLCanvas):
         y_step = 50 #if the screen is very crowded or empty could adjust this for readability
 
         for j in range(len(outputs)):
-            self.render_text(self.output_labels[j], 10, y_spacing*(2*j + 1) + 20)
+            self.render_text(self.output_labels[j], 50, y_spacing*(2*j + 1) + y_step*3/2)
+            self.render_text('0', 25, y_spacing*(2*j + 1))
+            self.render_text('1', 25, y_spacing*(2*j + 1) + y_step)
             GL.glColor3f(0.0, 0.0, 1.0)  # signal trace is blue
             GL.glBegin(GL.GL_LINE_STRIP)
             for i in range(length):
