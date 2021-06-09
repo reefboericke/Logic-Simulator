5ba122c70404745a2374174b56552a759515d9a8
j-m-nash <68859468+j-m-nash@users.noreply.github.com>
2021-06-09 10:05:01 +0100

    make code PEP 8 compliant

--- sample_parser_usage.py
+++ sample_parser_usage.py
@@ -13,17 +13,19 @@ import linecache
 
 
 def run():
-    
+
     names = Names()
     devices = Devices(names)
     network = Network(names, devices)
     monitors = Monitors(names, devices, network)
-    
-    scanner = Scanner('/Users/finnashley/Onedrive/University/IIA/GF2/Logic-Simulator/comment_only.bna', names)
+
+    scanner = Scanner('comment_only.bna', names)
     error_db = Error_Store(scanner)
-    parser = Parser(names,devices,network,monitors,scanner, error_db)
+    parser = Parser(names, devices, network,
+                    monitors, scanner, error_db)
 
     parser.parse_network()
+
 
 if __name__ == "__main__":
     run()
